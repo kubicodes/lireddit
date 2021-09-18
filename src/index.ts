d8d416f74549862a439fe694864b888655204d7f
kubicodes <kubicodes@gmail.com>
2021-09-18 21:47:55 +0200

    Setup Express and Apollo Graphql Server

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
 import { MikroORM } from "@mikro-orm/core";
-// import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up(); //run migrations before doing anything else
 
-  /** Code Example to Insert an entry using an entity
-   ** const post = orm.em.create(Post, { title: "my first post" }); //doesn´t insert to db
-   ** await orm.em.persistAndFlush(post);
-   **/
+  const app = express();
 
-  /** Code Example to fetch all entries for an entity
-   ** const posts = await orm.em.find(Post, {});
-   ** console.log(posts);
-   **/
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+  /**
+   * Create a GraphQl Endpoint on Express
+   */
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("Server started on localhost:4000");
+  });
 };
 
 main().catch((error) => console.log(error));
