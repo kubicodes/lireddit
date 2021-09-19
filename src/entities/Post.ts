788c4a40a87efbc59374cc0fa635c135b63c54a5
kubicodes <kubicodes@gmail.com>
2021-09-19 10:32:45 +0200

    Implement CRUD functionality for Post entity

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,16 +1,22 @@
 import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType() //declare as graphql object type
 @Entity()
 export class Post {
+  @Field(() => Int) //declare as grapqhl field. if not declared, it won´t be available in graphql
   @PrimaryKey()
   id!: number;
 
+  @Field(() => String)
   @Property({ type: "date" })
   createdAt: Date = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt: Date = new Date();
 
+  @Field()
   @Property({ type: "text" })
   title!: string;
 }
