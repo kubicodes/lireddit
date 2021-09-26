5e36d9f7f0ba884d9c488a0a916f57b11d6de25a
kubicodes <kubilayercikti1@gmail.com>
2021-09-26 16:18:29 +0200

    registering user from register page using urql

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -3,16 +3,33 @@ import { Wrapper } from "../components/Wrapper";
 import { Form, Formik } from "formik";
 import { InputField } from "../components/InputField";
 import { Box, Button } from "@chakra-ui/react";
+import { useMutation } from "urql";
 
 interface RegisterProps {}
 
+const REGISTER_MUTATION = `
+mutation Register($username: String!, $password:String!) {
+  register(options: { username: $username, password: $password }) {
+    errors {
+      field
+      message
+    }
+    user {
+      id
+      username
+    }
+  }
+}
+`;
+
 const Register: React.FC<RegisterProps> = () => {
+  const [, register] = useMutation(REGISTER_MUTATION);
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
-          console.log(values);
+        onSubmit={async (values) => {
+          const response = await register(values);
         }}
       >
         {({ isSubmitting }) => (
--- server/src/index.ts
+++ server/src/index.ts
@@ -11,6 +11,7 @@ import redis from "redis";
 import session from "express-session";
 import { __prod__ } from "./constants";
 import { MyContext } from "./types";
+import cors from "cors";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -21,6 +22,12 @@ const main = async () => {
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
 
+  app.use(
+    cors({
+      origin: "http://localhost:3000",
+      credentials: true,
+    })
+  );
   app.use(
     session({
       name: "qid",
@@ -45,14 +52,14 @@ const main = async () => {
       resolvers: [PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({ req, res }): MyContext => <MyContext>{ em: orm.em, req, res },
+    context: ({ req, res }) => <MyContext>{ em: orm.em, req, res },
   });
 
   await apolloServer.start();
   /**
    * Create a GraphQl Endpoint on Express
    */
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({ app, cors: false });
 
   app.listen(4000, () => {
     console.log("Server started on localhost:4000");
