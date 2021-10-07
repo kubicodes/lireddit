15068191c0d70b60692c0f3f496b181d6e53cb6b
kubicodes <kubilayercikti1@gmail.com>
2021-10-07 22:20:43 +0200

    add relationships between user and posts entities and adjust resolver for creating a post

--- server/src/entities/Post.ts
+++ server/src/entities/Post.ts
@@ -4,9 +4,11 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { User } from "./User";
 
 @ObjectType() //declare as graphql object type
 @Entity()
@@ -16,14 +18,29 @@ export class Post extends BaseEntity {
   id!: number;
 
   @Field()
+  @Column()
+  title!: string;
+
+  @Field()
+  @Column()
+  text!: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  creator!: User;
+
+  @Field()
+  @Column()
+  creatorId!: number;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  points!: number;
+
+  @Field()
   @CreateDateColumn()
   createdAt: Date;
 
   @Field()
   @UpdateDateColumn()
   updatedAt: Date;
-
-  @Field()
-  @Column()
-  title!: string;
 }
--- server/src/entities/User.ts
+++ server/src/entities/User.ts
@@ -3,10 +3,11 @@ import {
   BaseEntity,
   Column,
   CreateDateColumn,
-  Entity,
+  Entity, OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
+import {Post} from "./Post";
 
 @ObjectType()
 @Entity()
@@ -14,14 +15,6 @@ export class User extends BaseEntity {
   @Field()
   @PrimaryGeneratedColumn()
   id!: number;
-
-  @Field()
-  @CreateDateColumn()
-  createdAt: Date;
-
-  @Field()
-  @UpdateDateColumn()
-  updatedAt: Date;
 
   @Field()
   @Column({ unique: true })
@@ -33,4 +26,15 @@ export class User extends BaseEntity {
 
   @Column()
   password!: string;
+
+  @OneToMany(() => Post, post => post.creator)
+  posts: Post[]
+
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field()
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
