e04b20c78f31c7777dadcef32478dcf98939c92b
kubicodes <kubilayercikti1@gmail.com>
2021-10-10 13:05:26 +0200

    add global error handling to urql client

--- client/src/utils/createUrqlClient.ts
+++ client/src/utils/createUrqlClient.ts
@@ -1,4 +1,4 @@
-import { createClient, dedupExchange, fetchExchange } from "urql";
+import { createClient, dedupExchange, Exchange, fetchExchange } from "urql";
 import { cacheExchange } from "@urql/exchange-graphcache";
 import {
   LoginMutation,
@@ -8,6 +8,21 @@ import {
   RegisterMutation,
 } from "../generated/graphql";
 import { typeSafeUpdateQuery } from "./typeSafeUpdateQuery";
+import { pipe, tap } from "wonka";
+import { Router } from "next/router";
+
+const errorExchange: Exchange =
+  ({ forward }) =>
+  (ops$) => {
+    return pipe(
+      forward(ops$),
+      tap(({ error }) => {
+        if (error?.message.includes("not authenticated")) {
+          Router.replace("/login");
+        }
+      })
+    );
+  };
 
 export const createUrqlClient = (ssrExchange: any) => ({
   url: "http://localhost:4000/graphql",
@@ -62,6 +77,7 @@ export const createUrqlClient = (ssrExchange: any) => ({
         },
       },
     }),
+    errorExchange,
     ssrExchange,
     fetchExchange,
   ],
