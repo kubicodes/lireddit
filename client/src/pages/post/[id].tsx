a58115dcd15987ea567dc0c904826eafc9fb567f
kubicodes <kubilayercikti1@gmail.com>
2021-10-14 18:35:02 +0200

    add page to display single post

---
+++ client/src/pages/post/[id].tsx
@@ -0,0 +1,53 @@
+import { Heading } from "@chakra-ui/layout";
+import { withUrqlClient } from "next-urql";
+import { useRouter } from "next/router";
+import React from "react";
+import { Layout } from "../../components/Layout";
+import { usePostQuery } from "../../generated/graphql";
+import { createUrqlClient } from "../../utils/createUrqlClient";
+
+const Post = ({}) => {
+  const router = useRouter();
+  const postId =
+    typeof router.query.id === "string" ? parseInt(router.query.id) : -1;
+
+  const [{ data, fetching, error }] = usePostQuery({
+    pause: postId === -1,
+    variables: {
+      id: postId,
+    },
+  });
+
+  if (fetching) {
+    return (
+      <Layout>
+        <div>Loading...</div>
+      </Layout>
+    );
+  }
+
+  if (error) {
+    return (
+      <Layout>
+        <div>{error.message}</div>
+      </Layout>
+    );
+  }
+
+  if (!data?.post) {
+    return (
+      <Layout>
+        <div>Could not find post with Id {postId}</div>
+      </Layout>
+    );
+  }
+
+  return (
+    <Layout>
+      <Heading mb={4}>{data.post.title}</Heading>
+      {data.post.text}
+    </Layout>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Post);
